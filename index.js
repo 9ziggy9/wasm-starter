ef6c4d8f40b9f6409a372f326922a1042159c655
9ziggy9 <davidarogers@protonmail.com>
2022-07-10 22:05:45 -0700

    wasm correctly compiles, looks good

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 async function start() {
   console.log("Started!");
+  const wasm = await WebAssembly
+	.instantiateStreaming(fetch('./wasm/hello.wasm'));
+  // Here are our wasm functions!
+  console.log(wasm.instance.exports);
+  const {hello} = wasm.instance.exports;
+  const response = hello(34,35);
+  console.log(response);
 }
 
 start().catch(e => console.error(e));
